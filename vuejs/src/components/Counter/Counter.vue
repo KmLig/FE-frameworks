13dbec6b805402c259c7160a2152685d46a0023f
Khiem <phamxuankhiem1102@gmail.com>
2025-10-05 22:37:44 +0700

    Templating - Event click - Vue.js

---
+++ vuejs/src/components/Counter/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <p>Counter: {{ count }}</p>
+    <button @click="incrementCount">+1</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+
+function incrementCount() {
+    count.value++
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
