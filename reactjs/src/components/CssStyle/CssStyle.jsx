34c1fb0a225a536f4ef2264ae549f67f13d9631b
Khiem <phamxuankhiem1102@gmail.com>
2025-10-05 22:01:46 +0700

    Templating - Styling - Reactjs

---
+++ reactjs/src/components/CssStyle/CssStyle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CssStyle.css"
+export default function CssStyle() {
+  return (
+    <>
+        <h1 className='title'>I am red</h1>
+        <button style={{ fontSize: "10rem"}}>I am a button</button>
+    </>
+  )
+}
