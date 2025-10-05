3cb821c09a796a0bf1a69b1a09519be25d165b39
Khiem <phamxuankhiem1102@gmail.com>
2025-10-05 22:28:18 +0700

    Templating - Event click - Reactjs

---
+++ reactjs/src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    function incrementCount() {
+        setCount((count) => count + 1);
+    }
+  return (
+    <>
+        <p>Counter: {count}</p>
+        <button onClick={incrementCount}>+1</button>
+    </>
+  )
+}
