ef0a138e0a53367a717d5d5e45cef6702a6121ec
Khiem <phamxuankhiem1102@gmail.com>
2025-10-05 22:24:08 +0700

    Templating - Loop - Vue.js

---
+++ vuejs/src/components/Colors/Colors.vue
@@ -0,0 +1,16 @@
+<template>
+   <ul>
+    <li v-for="color in colors" :key="color">
+        {{ color }}
+    </li>
+   </ul>
+</template>
+
+<script setup>
+const colors = ['red', 'green', 'blue']
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
