fd1fba8b1111ba63039c490249cc0cae7c595dd0
Khiem <phamxuankhiem1102@gmail.com>
2025-10-05 22:17:42 +0700

    Templating - Styling - Vue.js

---
+++ vuejs/src/components/CssStyle.vue/CssStyle.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="title">I am red</h1>
+    <button style="font-size: 10rem;">I am a button</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+$title-color: red;
+
+.title {
+    color: $title-color;
+
+    &:hover {
+        color: darken($title-color, 20%);
+    }
+}
+
+</style>
