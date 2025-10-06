0297b56d34d738714e9adcdb574a761078f1d7e2
Khiem <phamxuankhiem1102@gmail.com>
2025-10-06 19:32:09 +0700

    Templating - Dom ref - Vue.js

---
+++ vuejs/src/components/InputFocused/InputFocused.vue
@@ -0,0 +1,16 @@
+<template>
+    <input type="text" ref="inputElement"/>
+</template>
+
+<script setup>
+import { onMounted, useTemplateRef } from 'vue';
+const inputElement = useTemplateRef("inputElement")
+
+onMounted(() => {
+    inputElement.value.focus()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
