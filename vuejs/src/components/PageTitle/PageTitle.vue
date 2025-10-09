75437a8fe20f06fc7442ebc00bc2e347887ba2b2
Khiem <phamxuankhiem1102@gmail.com>
2025-10-09 23:08:45 +0700

    Lifecycle - On mount - Vue.js

---
+++ vuejs/src/components/PageTitle/PageTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>Page title: {{ pageTitle }}</p>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const pageTitle = ref('')
+
+onMounted(() => {
+    pageTitle.value = document.title || 'Hi'
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>        
